4b75b1ff91fb772b6829f96d0226360d5ac24717
Nandini <nandiniverma202021@gmail.com>
2026-06-15 20:10:55 +0530

    create models and connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import "dotenv/config";
+import cors from "cors";
+import { connectDB } from './lib/db.js';
+
+import { clerkMiddleware } from '@clerk/express';
 
 const app=express();
 const PORT=process.env.PORT || 3000;
+const FRONTEND_URL=process.env.FRONTEND_URL;
 
+
+// Middlewares
+app.use(express.json());
+app.use(cors({origin:FRONTEND_URL, credentials:true}));
+app.use(clerkMiddleware());
+
+
+app.get("/health", (req,res)=>{
+    res.status(200).json({ok:true});
+});
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 })
