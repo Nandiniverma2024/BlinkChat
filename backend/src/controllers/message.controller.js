af213cd27ac3245ab4b17bf1896e6ce2a6b8bb74
Nandini <nandiniverma202021@gmail.com>
2026-06-26 20:26:24 +0530

    feat: import socket utilities for real-time messaging support

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import { uploadChatMedia, hasImageKitConfig } from "../lib/imagekit.js";
-
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 
 export async function getUsersForSidebar(req, res) {
